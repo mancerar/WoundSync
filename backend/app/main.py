8a4910554de0e70ac227d2b0e4f0a708493da51a
Ryan Mancera <mancerar@mcmaster.ca>
2026-04-05 12:30:20 -0400

    Cloudflare backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -68,6 +68,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://wound-sync.org",
+        "https://www.wound-sync.org",
 
         # Capacitor / iOS WebView origins:
         "capacitor://localhost",
